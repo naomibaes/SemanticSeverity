3f45a769b0192431cd8afec6c51d381d88db9cda
Kat Vylomova <evylomova@gmail.com>
2022-07-05 18:11:02 +1000

    Update preprocess_data_spacy.py

--- scripts/preprocess_data_spacy.py
+++ scripts/preprocess_data_spacy.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-# os.chdir('/home/localadmin/Documents/Spartan/')
 import os
 #import pandas as pd
 import re, spacy
